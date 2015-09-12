d36e6fb3fc5ea733748af031590328633ddba536
Mr.wang <Mr.wang@172.30.113.4>
2015-09-12 10:19:32 +0800

    1

---
+++ src/copy/NestedPanels.java
@@ -0,0 +1,32 @@
+package copy;
+import java.awt.*;
+import javax.swing.*;
+
+public class NestedPanels {
+  public static void main (String[]args){
+	  JFrame frame=new JFrame ("Nested Panels");
+	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  JPanel subPanel1=new JPanel();
+	  subPanel1.setPreferredSize(new Dimension(150,100));
+	  subPanel1.setBackground(Color.green);
+	  JLabel label1=new JLabel("YES");
+	  subPanel1.add(label1);
+	  
+	  
+	  JPanel subPanel2=new JPanel();
+	  subPanel2.setPreferredSize(new Dimension(150,100));
+	  subPanel2.setBackground(Color.red);
+	  JLabel label2=new JLabel("NO");
+	  subPanel2.add(label2);
+	  subPanel1.add(label2);
+	  
+	  JPanel primary=new JPanel();
+	  primary.setBackground(Color.blue);
+	  primary.add(subPanel1);
+	  primary.add(subPanel2);
+	  frame.getContentPane().add(primary);
+	  frame.pack();
+	  frame.setVisible(true);
+	  
+  }
+}
